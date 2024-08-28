b201e204274600a84efdfc16bf1273921dac1729
Felipe Benedetti <felipe-benedetti@hotmail.com>
2024-08-27 22:50:07 -0300

    Template

---
+++ README.md
@@ -0,0 +1,37 @@
+
+Responsive Template Header with Hamburger Menu in React
+This project is a responsive header component built with React. It features a hamburger menu that adapts to different screen sizes, providing a smooth user experience on both mobile and desktop devices.
+
+Features
+Responsive Design: The header adjusts its layout depending on the screen size.
+Hamburger Menu: A collapsible menu for mobile devices.
+Smooth Animations: CSS transitions for a polished user experience.
+React Hooks: Modern React practices using hooks for state management.
+Installation
+Clone the repository:
+
+bash
+
+git clone https://github.com/FelipeBenedetti/responsive-react-navbar.git
+cd responsive-header-react
+Install dependencies:
+
+Make sure you have Node.js installed, then run:
+
+bash
+
+npm install
+Run the application:
+
+Start the development server:
+
+bash
+
+npm start
+The app should open in your default browser at http://localhost:3000.
+
+Usage
+Desktop View: The full menu is displayed horizontally across the header.
+Mobile View: The menu collapses into a hamburger icon. Clicking the icon expands the menu vertically.
+Customization: You can easily customize the styles by modifying the CSS files.
+Project Structure
---
+++ src/components/Header/NavBar.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import { HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
+import Logo from './Logo/semfundologo2.png'
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="logof">
+            <img src={Logo} alt="Logo" className="logob" />
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Sobre
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/blog"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contato
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {click ? (
+              <span className="icon">
+                <HamburgetMenuClose />{" "}
+              </span>
+            ) : (
+              <span className="icon">
+                <HamburgetMenuOpen />
+              </span>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
